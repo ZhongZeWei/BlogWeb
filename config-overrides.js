f30d9d23b5fc6997b83e4bd54e73723595357b79
ZhongZeWei <2205101603@qq.com>
2019-01-31 15:57:03 +0800

    第二天：实现user注册、登录、注册成功的界面

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,23 @@
-const {
-    override,
-    fixBabelImports
-} = require('customize-cra');
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
 
 module.exports = override(
     fixBabelImports('import', {
-        libraryName: 'antd',
-        libraryDirectory: 'es',
-        style: 'css',
+      libraryName: 'antd',
+      libraryDirectory: 'es',
+      style: true,
     }),
-);
+   addLessLoader({
+     javascriptEnabled: true,
+     modifyVars: { '@primary-color': '#1DA57A' },
+   }),
+  );
+
+//用@定义src目录
+const path = require('path')
+module.exports = function override(config, env) {
+    config.resolve.alias = {
+        ...config.resolve.alias,
+        '@': path.resolve(__dirname, 'src'),
+    };
+    return config;
+};
--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
-import { Button } from 'antd';
+
 import './App.css';
+//引入路由依赖
+import { BrowserRouter as Router, Route } from "react-router-dom";
+//开始引入页面js
+import Login from './pages/user/login'
+import Register from './pages/user/Register'
+import RegisterResult from './pages/user/RegisterResult'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Button type="primary">Button</Button>
-      </div>
+           <Router>  
+             <div className="page">
+             <Route exact path="/user" component={Login} />
+              <Route exact path="/user/login" component={Login} />
+              <Route exact path="/user/register" component={Register} />
+              <Route exact path="/user/register-result" component={RegisterResult} />
+               </div> 
+            
+               
+          
+          </Router>
+
+     </div>
+
     );
   }
 }
